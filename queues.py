3764595dd54e4c68fd1bf0383b2679ad744b9a8e
Romeo <romeogescolano@gmail.com>
2022-12-11 20:12:42 +0800

    comment: codes to access the Queue class

--- queues.py
+++ queues.py
@@ -9,4 +9,18 @@ class Queue:
          
     def dequeue(self):
         return self._elements.popleft()
-         
+    
+# from queues import Queue
+
+# create object
+# fifo = Queue()
+
+# access methods by putting item on queue
+# fifo.enqueue(1)
+# fifo.enqueue(2)
+# fifo.dequeue(3)
+
+# remove the first item on queue
+# fifo.dequeue(1)
+# fifo.dequeue(2)
+# fifo.dequeue(3)
