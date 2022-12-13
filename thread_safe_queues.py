6fa45a22df816ec34d7f7127b81b267449b682af
Romeo <romeogescolano@gmail.com>
2022-12-14 00:23:50 +0800

    add: priority queue in thread_safe_queue.py

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -141,8 +141,9 @@ class View:
           
 def main(args):
     buffer = QUEUE_TYPES[args.queue]()
+    products = PRIORITIZED_PRODUCTS if args.queue == "heap" else PRODUCTS
     producers = [
-        Producer(args.producer_speed, buffer, PRODUCTS)
+        Producer(args.producer_speed, buffer, products)
         for _ in range(args.producers)
     ]
     consumers = [
@@ -173,4 +174,22 @@ if __name__ == "__main__":
         main(parse_args())
     except KeyboardInterrupt:
         pass
-    
+
+@dataclass(order=True)
+class Product:
+    priority: int
+    label: str = field(compare=False)
+
+    def __str__(self):
+        return self.label
+
+class Priority(IntEnum):
+    HIGH = 1
+    MEDIUM = 2
+    LOW = 3
+
+PRIORITIZED_PRODUCTS = (
+    Product(Priority.HIGH, ":1st_place_medal:"),
+    Product(Priority.MEDIUM, ":2nd_place_medal:"),
+    Product(Priority.LOW, ":3rd_place_medal:"),
+)
