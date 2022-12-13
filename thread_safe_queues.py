e48ee505867295ec8e8b5d23169839f5c912935f
Romeo <romeogescolano@gmail.com>
2022-12-14 00:13:30 +0800

    fix: indention of property in worker class

--- thread_safe_queues.py
+++ thread_safe_queues.py
@@ -50,25 +50,25 @@ class Worker(threading.Thread):
         self.working = False
         self.progress = 0
         
-        @property
-        def state(self):
-            if self.working:
-                return f"{self.product} ({self.progress}%)"
-            return ": zzz: idle"
+    @property
+    def state(self):
+        if self.working:
+            return f"{self.product} ({self.progress}%)"
+        return ": zzz: idle"
         
-        def simulate_idle(self):
-            self.product = None
-            self.working = False
-            self.progress = 0
-            sleep(randint(1, 3))
+    def simulate_idle(self):
+        self.product = None
+        self.working = False
+        self.progress = 0
+        sleep(randint(1, 3))
         
-        def simulate_work(self):
-            self.working = True
-            self.progress = 0
-            delay = randint(1, 1 + 15 // self.speed)
-            for _ in range(100):
-                sleep(delay/100)
-                self.progress += 1
+    def simulate_work(self):
+        self.working = True
+        self.progress = 0
+        delay = randint(1, 1 + 15 // self.speed)
+        for _ in range(100):
+            sleep(delay/100)
+            self.progress += 1
                 
 class Producer(Worker):
     def __init__(self, speed, buffer, products):
@@ -137,18 +137,8 @@ class View:
             padding + worker.state, align="left", vertical="middle"
         )
         return Panel(align, height=5, title=title)
-    
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-q", "--queue", choices = QUEUE_TYPES, default = "fifo")
-    parser.add_argument("-p", "--producers", type = int, default = 3)
-    parser.add_argument("-c", "--consumers", type = int, default = 2)
-    parser.add_argument("-ps", "--producer-speed", type = int, default = 1)
-    parser.add_argument("-cs", "--consumer-speed", type = int, default = 1)
-    return parser.parse_args()
-
-       
+          
+          
 def main(args):
     buffer = QUEUE_TYPES[args.queue]()
     producers = [
@@ -168,6 +158,16 @@ def main(args):
     view = View(buffer, producers, consumers)
     view.animate()
     
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-q", "--queue", choices = QUEUE_TYPES, default = "fifo")
+    parser.add_argument("-p", "--producers", type = int, default = 3)
+    parser.add_argument("-c", "--consumers", type = int, default = 2)
+    parser.add_argument("-ps", "--producer-speed", type = int, default = 1)
+    parser.add_argument("-cs", "--consumer-speed", type = int, default = 1)
+    return parser.parse_args()
+
+ 
 if __name__ == "__main__":
     try:
         main(parse_args())
