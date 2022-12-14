2462a720f2c91b0d506959100b81674e198f90bf
Romeo <romeogescolano@gmail.com>
2022-12-15 00:19:26 +0800

    update md5 reversing function

--- multiprocess_queue.py
+++ multiprocess_queue.py
@@ -1,11 +1,10 @@
 import time
 from hashlib import md5
-from itertools import product
 from string import ascii_lowercase
 
 def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
     for length in range(1, max_length + 1):
-        for combination in product(alphabet, repeat=length):
+        for combination in Combinations(alphabet, length):
             text_bytes = "".join(combination).encode("utf-8")
             hashed = md5(text_bytes).hexdigest()
             if hashed == hash_value:
